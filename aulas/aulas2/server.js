7745cb0cb0d4ce3909b5aab22319a74820f06752
Heryk Ribeiro <herykribeiro054@gmail.com>
2026-04-27 21:52:58 -0300

    Colocando o Dockerfile na raiz

--- aulas/aulas2/server.js
+++ aulas/aulas2/server.js
@@ -3,9 +3,9 @@ const app = express();
 const PORT = 3000;
 
 app.get('/', (req, res) => {
-  res.json({ mensagem: "Olá do meu próprio Container Docker!", status: "Sucesso" });
-});
+  res.json({ mensagem: "Olá do meu próprio Container Docker!", status: "Sucesso" });});
 
 app.listen(PORT, () => {
-  console.log(`API rodando na porta ${PORT}`);
-});
+  console.log(`API rodando na porta ${PORT}`);});
+
+//teste para o git
